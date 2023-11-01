a58e465efb403762049ec898a403ac97770ca626
Dinh Bach Nguyen <95115146+bachsuperman@users.noreply.github.com>
2023-11-01 15:12:15 +1100

    Update with the radius of the ellisoid
    Jarred has not been determined the right radii for the eliipsoid for each link so I have to do it.

--- ellipsoidCollisionChecking.m
+++ ellipsoidCollisionChecking.m
@@ -11,9 +11,9 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
 
     % Plot Ellipsoid Around Robot End Effector
     centerPoint = [x y z];
-    radii = [0.1,0.2,0.3];
-    [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-    ellipsoidEndEffector_h = surf(X,Y,Z,'FaceAlpha',0.1); % Make the ellipsoid translucent (so we can see the inside and outside points)
+    radii = [0.2,0.3,0.15];
+    [XEE,YEE,ZEE] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
+    ellipsoidEndEffector_h = surf(XEE,YEE,ZEE,'FaceAlpha',0.1,'EdgeColor','flat'); % Make the ellipsoid translucent (so we can see the inside and outside points)
     drawnow();
 
     % Test if any vertices of the rectangular prism are inside the ellipsoid
@@ -22,7 +22,7 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
 
     if pointsInside > 0
         check = 1;
-        logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
+        logMessage = sprintf(num2str(size(pointsInside,1)),' points inside the ellipsoid');
     end
     
     [proxy, linkPoses] = robot.model.fkine(robot.model.getpos());
@@ -38,34 +38,31 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
     zlink1 = linkPose1(3,4);
 
     % Create Ellipsoid Around Robot Link 1
-    centerPoint = [xlink1 ylink1 zlink1];
-    radii = [0.1,0.2,0.3];
-    [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-
+    % centerPoint = [xlink1 ylink1 zlink1];
+    % radii = [0.5,0.4,0.1];
+    % [X1,Y1,Z1] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
+    % surf(X1,Y1,Z1,'FaceAlpha',0.01,'EdgeColor','none'); % Make the ellipsoid translucent (so we can see the inside and outside points)
+    % drawnow();
     % Test if any vertices of the rectangular prism are inside the
     % ellipsoid
-    algebraicDist = GetAlgebraicDist(points, centerPoint, radii);
-    pointsInside = find(algebraicDist < 1);
-
-    if pointsInside > 0
-        check = 1;
-        logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
-    end
-
+    % algebraicDist = GetAlgebraicDist(points, centerPoint, radii);
+    % pointsInside = find(algebraicDist < 1);
+    % 
+    % if pointsInside > 0
+    %     check = 1;
+    %     logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
+    % end
+    % 
     xlink2 = linkPose2(1,4);
     ylink2 = linkPose2(2,4);
     zlink2 = linkPose2(3,4);
 
     % Plot Ellipsoid Around Robot Link2
     centerPoint = [xlink2 ylink2 zlink2];
-    radii = [0.1,0.2,0.3];
-    [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-    % ellipsoidLink2_h = surf(X,Y,Z,'FaceAlpha',0.1);
-    % 
+    radii = [0.5,0.4,0.35];
+    % [X2,Y2,Z2] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
+    % ellipsoidLink2 = surf(X2,Y2,Z2,'FaceAlpha',0.05,'EdgeColor','interp');
     % drawnow();
-    % Make the ellipsoid translucent (so we can see the inside and outside points)
-    % alpha(0.1);
-
     % Test if any vertices of the rectangular prism are inside the
     % ellipsoid
     algebraicDist = GetAlgebraicDist(points, centerPoint, radii);
@@ -73,7 +70,7 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
 
     if pointsInside > 0
         check = 1;
-        logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
+        logMessage = sprintf(num2str(size(pointsInside,1)),' points inside the ellipsoid of Link2');
     end
 
     xlink3 = linkPose3(1,4);
@@ -81,10 +78,11 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
     zlink3 = linkPose3(3,4);
 
     % Create Ellipsoid Around Robot End Link 3
-    centerPoint = [xlink3 ylink3 zlink3];
-    radii = [0.1,0.2,0.3];
-    [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-
+    centerPoint = [xlink3+0.15 ylink3 zlink3];
+    radii = [0.2,0.2,0.5];
+    % [X3,Y3,Z3] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
+    % surf(X3,Y3,Z3,'FaceAlpha',0.1,'EdgeColor','none');
+    % drawnow();
     % Test if any vertices of the rectangular prism are inside the
     % ellipsoid
     algebraicDist = GetAlgebraicDist(points, centerPoint, radii);
@@ -92,7 +90,7 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
 
     if pointsInside > 0
         check = 1;
-        logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
+        logMessage = sprintf(num2str(size(pointsInside,1)),' points inside the ellipsoid of Link3');
     end
 
     xlink4 = linkPose4(1,4);
@@ -100,16 +98,17 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
     zlink4 = linkPose4(3,4);
 
     % Plot Ellipsoid Around Robot Link 4
-    centerPoint = [xlink4 ylink4 zlink4];
-    radii = [0.1,0.2,0.3];
-    [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-
+    centerPoint = [xlink4+0.15 ylink4 zlink4];
+    radii = [0.2,0.4,0.2];
+    % [X4,Y4,Z4] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
+    % surf(X4,Y4,Z4,'FaceAlpha',0.1,'EdgeColor','none');
+    % drawnow();
     algebraicDist = GetAlgebraicDist(points, centerPoint, radii);
     pointsInside = find(algebraicDist < 1);
 
     if pointsInside > 0
         check = 1;
-        logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
+        logMessage = sprintf(num2str(size(pointsInside,1)),' points inside the ellipsoid');
     end
     
     xlink5 = linkPose5(1,4);
@@ -118,15 +117,15 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
 
     % Create Ellipsoid Around Robot Link 5
     centerPoint = [xlink5 ylink5 zlink5];
-    radii = [0.1,0.2,0.3];
-    [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-
+    radii = [0.3,0.4,0.2];
+    % [X5,Y5,Z5] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
+    % ellipsoidLink5 = surf(X5,Y5,Z5,'FaceAlpha',0.1,'EdgeColor','none');
     algebraicDist = GetAlgebraicDist(points, centerPoint, radii);
     pointsInside = find(algebraicDist < 1);
-
+    % drawnow();
     if pointsInside > 0
         check = 1;
-        logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
+        logMessage = sprintf(num2str(size(pointsInside,1)),' points inside the ellipsoid');
     end    
 
     xlink6 = linkPose6(1,4);
@@ -135,18 +134,23 @@ function [check logMessage] = ellipsoidCollisionChecking(robot, points)
 
     % Create Ellipsoid Around Robot Link 6
     centerPoint = [xlink6 ylink6 zlink6];
-    radii = [0.1,0.2,0.3];
-    [X,Y,Z] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
-
+    radii = [0.3,0.4,0.2];
+    % [X6,Y6,Z6] = ellipsoid( centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3) );
+    % ellipsoidLink6 = surf(X6,Y6,Z6,'FaceAlpha',0.1,'EdgeColor','none');
     algebraicDist = GetAlgebraicDist(points, centerPoint, radii);
     pointsInside = find(algebraicDist < 1);
-
+    % drawnow();
     if pointsInside > 0
         check = 1;
-        logMessage = sprintf('There are ', num2str(size(pointsInside,1)),' points inside the ellipsoid');
+        logMessage = sprintf(num2str(size(pointsInside,1)),' points inside the ellipsoid');
     end
 
     try delete(ellipsoidEndEffector_h); end
+    %try delete(ellipsoidLink2); end
+    % try delete(ellipsoidLink3); end
+    % try delete(ellipsoidLink4); end
+    % try delete(ellipsoidLink5); end
+    % try delete(ellipsoidLink6); end
 
 
 end
